4330bed49a0af8c4a8efa113902f5d5db92a7e36
Ryan DiMascio <ryand@channelbakers.com>
2020-07-26 20:41:32 -0700

    feat: add cjs and es6 module output

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,30 @@
+/* eslint-disable import/no-anonymous-default-export */
+
 import path from 'path';
 import {uglify} from 'rollup-plugin-uglify';
 import {getBabelOutputPlugin} from '@rollup/plugin-babel';
 
+import pkg from './package.json';
+
 const config = {
 	input: 'src/index.js',
+	plugins: [],
+	output: {
+		exports: 'default',
+		sourcemap: true
+	}
+};
+
+const umd = {
+	...config,
+	output: {
+		...config.output,
+		file: pkg.unpkg,
+		format: 'umd',
+		name: 'getAttributes'
+	},
 	plugins: [
+		...config.plugins,
 		getBabelOutputPlugin({
 			configFile: path.resolve(__dirname, '.babelrc'),
 			allowAllFormats: true
@@ -16,13 +36,23 @@ const config = {
 				unsafe_comps: true // eslint-disable-line camelcase
 			}
 		})
-	],
-	output: {
-		exports: 'default',
-		file: 'dist/index.min.js',
-		format: 'umd',
-		name: 'getAttributes'
-	}
+	]
 };
 
-export default config;
+const web = {
+	...config,
+	output: [
+		{
+			...config.output,
+			file: pkg.main,
+			format: 'cjs'
+		},
+		{
+			...config.output,
+			file: pkg.module,
+			format: 'es'
+		}
+	]
+};
+
+export default [umd, web];
